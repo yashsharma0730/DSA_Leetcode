80576c5173079b2a0e06e86a5cc062eff030b79a
Yash Sharma <113891880+yashsharma3@users.noreply.github.com>
2025-06-10 09:33:20 +0530

    Time: 2 ms (94.65%) | Memory: 50 MB (78.10%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -4,7 +4,7 @@ class Solution {
         int count =0;
         for(int i =0;i<nums.length;i++){
             if(nums[i]==1){
-                count=count+1;
+                count+=1;
             if(count > maxcount){
                 maxcount=count;
             }
